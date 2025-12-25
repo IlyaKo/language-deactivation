2c5a9faed2420042945ad3c79a63935c26566bf9
Ilya Kochetkov <ilya-ko@bk.ru>
2025-12-26 00:30:14 +0400

    read all installed languages

--- WinForms/LanguageService.cs
+++ WinForms/LanguageService.cs
@@ -1,4 +1,5 @@
-﻿using System.Management.Automation;
+﻿using System.Globalization;
+using System.Management.Automation;
 
 namespace WinForms;
 
@@ -6,12 +7,51 @@ public abstract class LanguageService
 {
     public static IReadOnlyList<Language> GetInstalledLanguages()
     {
-        var result = new List<Language>();
+        var installedLanguages = new List<Language>();
 
-        foreach (InputLanguage language in InputLanguage.InstalledInputLanguages)
-            result.Add(new Language { Name = language.Culture.DisplayName, Tag = language.Culture.Name, Enabled = true });
+        var activeTags = InputLanguage.InstalledInputLanguages
+                                      .Cast<InputLanguage>()
+                                      .Select(l => l.Culture.Name)
+                                      .ToHashSet(StringComparer.OrdinalIgnoreCase);
 
-        return result.AsReadOnly();
+        using var powerShell = PowerShell.Create();
+        powerShell.AddScript("Get-InstalledLanguage");
+        var results = powerShell.Invoke();
+        powerShell.Commands.Clear();
+        if (powerShell.HadErrors || results.Count == 0)
+            ThrowScriptError(powerShell);
+
+        var languages = results[0].BaseObject as IEnumerable<object>
+                      ?? throw new InvalidOperationException("Failed to retrieve user language list.");
+
+        var systemTags = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        foreach (var language in languages)
+        {
+            var idProperty = language.GetType().GetField("LanguageId");
+            if (idProperty?.GetValue(language) is string tag)
+                systemTags.Add(tag);
+        }
+
+        var allTags = activeTags.Union(systemTags);
+        foreach (var tag in allTags)
+            try
+            {
+                var culture = CultureInfo.GetCultureInfo(tag);
+
+                installedLanguages.Add(new Language
+                {
+                    Name = culture.DisplayName,
+                    Tag = culture.Name,
+                    Enabled = activeTags.Contains(tag),
+                    UserInterface = culture.Name.Equals(CultureInfo.CurrentUICulture.Name, StringComparison.OrdinalIgnoreCase),
+                });
+            }
+            catch
+            {
+
+            }
+
+        return installedLanguages.AsReadOnly();
     }
 
     public static void EnableLanguage(string targetTag)
@@ -32,48 +72,44 @@ public abstract class LanguageService
                     break;
                 }
 
-        Task.Run(() =>
+        using var powerShell = PowerShell.Create();
+        powerShell.AddCommand("Get-WinUserLanguageList");
+        var results = powerShell.Invoke();
+        powerShell.Commands.Clear();
+        if (powerShell.HadErrors || results.Count == 0)
+            ThrowScriptError(powerShell);
+
+        var languages = results[0].BaseObject as IEnumerable<object>
+                      ?? throw new InvalidOperationException("Failed to retrieve user language list.");
+
+        if (languages.Count() <= 1)
+            throw new InvalidOperationException("You should not remove the last active language");
+
+        List<string> keepTags = [];
+        var foundLanguage = false;
+
+        foreach (var language in languages)
         {
-            using var powerShell = PowerShell.Create();
-            powerShell.AddCommand("Get-WinUserLanguageList");
-            var results = powerShell.Invoke();
-            powerShell.Commands.Clear();
-            if (powerShell.HadErrors || results.Count == 0)
-                ThrowScriptError(powerShell);
+            var property = language.GetType().GetProperty("LanguageTag");
+            if (property?.GetValue(language) is not string tag)
+                continue;
 
-            var languages = results[0].BaseObject as IEnumerable<object>
-                          ?? throw new InvalidOperationException("Failed to retrieve user language list.");
+            if (tag.Equals(targetTag, StringComparison.OrdinalIgnoreCase))
+                foundLanguage = true;
+            else
+                keepTags.Add(tag);
+        }
 
-            if (languages.Count() <= 1)
-                throw new InvalidOperationException("You should not remove the last active language");
+        if (!foundLanguage)
+            return;
 
-            List<string> keepTags = [];
-            var foundLanguage = false;
+        var script = $"Set-WinUserLanguageList -LanguageList '{string.Join("','", keepTags)}' -Force";
 
-            foreach (var language in languages)
-            {
-                var property = language.GetType().GetProperty("LanguageTag");
-                if (property?.GetValue(language) is not string tag)
-                    continue;
+        powerShell.AddScript(script);
+        powerShell.Invoke();
 
-                if (tag.Equals(targetTag, StringComparison.OrdinalIgnoreCase))
-                    foundLanguage = true;
-                else
-                    keepTags.Add(tag);
-            }
-
-            if (!foundLanguage)
-                return;
-
-            var script = $"Set-WinUserLanguageList -LanguageList '{string.Join("','", keepTags)}' -Force";
-
-            powerShell.AddScript(script);
-            powerShell.Invoke();
-
-            if (powerShell.HadErrors)
-                ThrowScriptError(powerShell);
-
-        }).Wait();
+        if (powerShell.HadErrors)
+            ThrowScriptError(powerShell);
     }
 
     private static void ThrowScriptError(PowerShell ps)
